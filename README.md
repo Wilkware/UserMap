5332a4e94bff5b013602efbbde810a931b931fac
Heiko Wilknitz <heiko.wilknitz@wilkware.de>
2023-12-06 17:18:55 +0100

    Release v1.0.20231206

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
-# UserMap
-IP-Symcon Modul
+# Farbeverlauf (Color Loop)
+
+[![Version](https://img.shields.io/badge/Symcon-PHP--Modul-red.svg)](https://www.symcon.de/service/dokumentation/entwicklerbereich/sdk-tools/sdk-php/)
+[![Product](https://img.shields.io/badge/Symcon%20Version-6.4%20%3E-blue.svg)](https://www.symcon.de/produkt/)
+[![Version](https://img.shields.io/badge/Modul%20Version-1.0.20231206-orange.svg)](https://github.com/Wilkware/UserMap)
+[![License](https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-green.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/)
+[![Actions](https://github.com/Wilkware/UserMap/workflows/Check%20Style/badge.svg)](https://github.com/Wilkware/UserMap/actions)
+
+Das Modul bietet die Möglichkeit, jedem Symcon-Benutzer direkt von der Konsole aus seinen eigenen Standortmarker auf einer globalen Karte (Symcon User Map Website) hinzuzufügen.  
+
+## Inhaltverzeichnis
+
+1. [Funktionsumfang](#user-content-1-funktionsumfang)
+2. [Voraussetzungen](#user-content-2-voraussetzungen)
+3. [Installation](#user-content-3-installation)
+4. [Einrichten der Instanzen in IP-Symcon](#user-content-4-einrichten-der-instanzen-in-ip-symcon)
+5. [Statusvariablen und Profile](#user-content-5-statusvariablen-und-profile)
+6. [WebFront](#user-content-6-webfront)
+7. [PHP-Befehlsreferenz](#user-content-7-php-befehlsreferenz)
+8. [Versionshistorie](#user-content-8-versionshistorie)
+
+### 1. Funktionsumfang
+
+Die Idee für das Modul stammt aus dem alten Symcon Forum bzw. aus dem HomeMatic Forum. Dort hat man es einfach und pragmatisch mit Google Maps umgesetzt. Der Nachteil ist das man es immer mit Nachrichten hin und her verwaltet hat.  
+Diese Ansatz kombiniert verschiedene Technologien um ein einfaches und selbst managebares Verfahren zum Verwalten des eigenen Standorts zu bieten.  
+
+* Auslieferung über eine sehr schlanken One-Pager via CDN (netlify)
+* Registrieren, Aktualisieren und Löschen über eine einfache REST API
+* Redaktinelle Möglichkeit bei Fehlern schnell einzugreifen
+* Verwaltung des eigenen Standorts und privater Links via Symcon Modul
+
+### 2. Voraussetzungen
+
+* IP-Symcon ab Version 6.4
+
+### 3. Installation
+
+* Über den Modul Store das Modul _User Map_ installieren.
+* Alternativ Über das Modul-Control folgende URL hinzufügen.  
+`https://github.com/Wilkware/UserMap` oder `git://github.com/Wilkware/UserMap.git`
+
+### 4. Einrichten der Instanzen in IP-Symcon
+
+* Unter 'Instanz hinzufügen' ist das _User Map_-Modul (Alias: _Benutzerkarte_) unter dem Hersteller '(Geräte)' aufgeführt.
+
+__Konfigurationsseite__:
+
+Einstellungsbereich:
+
+> Benutzerdaten ...
+
+Name                               | Beschreibung
+---------------------------------- | -----------------------------------------------------------------
+Benutzername (Forum)               | Eigener Benutzername (Nickname/Spitzname) wie im Forum! Bitte nichts Neues erfinden!!!
+Standort (Breitengrad, Längengrad) | Dein gewünschter Standort. Über den KOPIEREN Button können die im System hinterlegten Koordinaten übernommen werden.
+Links (nicht verpflichend)         | Wer will kann mehrere Links zu seiner Person mitgeben (Website, Github ...)
+
+_Aktionsbereich:_
+
+Aktion                  | Beschreibung
+----------------------- | ---------------------------------
+REGISTRIEREN            | Den eigenen Standort freigeben bzw. registrieren
+AKTUALISIEREN           | Update der daten, z.B. neuer Standort oder Links. ÄNDERUNG DES NAMENS IST NICHT ERLAUBT!
+LÖSCHEN                 | Standort wieder zurücknehmen bzw. öffentlich Löschen!
+
+### 5. Statusvariablen und Profile
+
+Es werden keine zusätzlichen Statusvariablen oder Profile benötigt.
+
+### 6. WebFront
+
+Es ist keine weitere Steuerung oder gesonderte Darstellung integriert.
+
+### 7. PHP-Befehlsreferenz
+
+Das Modul stellt keine direkten Funktionsaufrufe zur Verfügung.
+
+### 8. Versionshistorie
+
+v1.0.20231206
+
+* _NEU_: Initialversion
+
+## Entwickler
+
+Seit nunmehr über 10 Jahren fasziniert mich das Thema Haussteuerung. In den letzten Jahren betätige ich mich auch intensiv in der IP-Symcon Community und steuere dort verschiedenste Skript und Module bei. Ihr findet mich dort unter dem Namen @pitti ;-)
+
+[![GitHub](https://img.shields.io/badge/GitHub-@wilkware-181717.svg?style=for-the-badge&logo=github)](https://wilkware.github.io/)
+
+## Spenden
+
+Die Software ist für die nicht kommerzielle Nutzung kostenlos, über eine Spende bei Gefallen des Moduls würde ich mich freuen.
+
+[![PayPal](https://img.shields.io/badge/PayPal-spenden-00457C.svg?style=for-the-badge&logo=paypal)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8816166)
+
+## Lizenz
+
+Namensnennung - Nicht-kommerziell - Weitergabe unter gleichen Bedingungen 4.0 International
+
+[![Licence](https://img.shields.io/badge/License-CC_BY--NC--SA_4.0-EF9421.svg?style=for-the-badge&logo=creativecommons)](https://creativecommons.org/licenses/by-nc-sa/4.0/)
---
+++ User Map/module.php
@@ -0,0 +1,272 @@
+<?php
+
+declare(strict_types=1);
+
+// Allgemeine Funktionen
+require_once __DIR__ . '/../libs/_traits.php';
+
+/**
+ * CLASS UserMap
+ */
+class UserMap extends IPSModule
+{
+    use DebugHelper;
+    use ProfileHelper;
+    use VariableHelper;
+
+    // ModulID (Location Control)
+    private const LOCATION_CONTROL_GUID = '{45E97A63-F870-408A-B259-2933F7EABF74}';
+    // WP REST API
+    private const WP_REST_URL = 'https://wilkware.de/wp-json/usermap/symcon';
+    private const WP_REST_APW = 'YXBwLnVzZXI6YXZDUSByMDNPIEU1Q1AgMUJFeiBOUDVKIGxtUVY=';
+
+    /**
+     * Overrides the internal IPSModule::Create($id) function
+     */
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
+        // User ID
+        $this->RegisterAttributeInteger('UserID', 0);
+        // Name (username) variable
+        $this->RegisterPropertyString('Name', '');
+        // Coordinates (LAT, LON)
+        $this->RegisterPropertyString('Coords', '{"latitude":0,"longitude":0}');
+        // Link List
+        $this->RegisterPropertyString('Links', '[]');
+    }
+
+    /**
+     * Overrides the internal IPSModule::Destroy($id) function
+     */
+    public function Destroy()
+    {
+        //Never delete this line!
+        parent::Destroy();
+    }
+
+    /**
+     * Configuration Form.
+     *
+     * @return JSON configuration string.
+     */
+    public function GetConfigurationForm()
+    {
+        // Get Form
+        $form = json_decode(file_get_contents(__DIR__ . '/form.json'), true);
+
+        $uid = $this->ReadAttributeInteger('UserID');
+        $name = $this->ReadPropertyString('Name');
+        $coords = json_decode($this->ReadPropertyString('Coords'), true);
+        // Buttons
+        if ($uid != 0) {
+            // Update
+            if (($name != '') && ($coords['latitude'] != 0) && ($coords['longitude'] != 0)) {
+                $form['actions'][1]['items'][1]['enabled'] = true;
+            }
+            // Delete
+            $form['actions'][1]['items'][2]['enabled'] = true;
+        } else {
+            if (($name != '') && ($coords['latitude'] != 0) && ($coords['longitude'] != 0)) {
+                $form['actions'][1]['items'][0]['enabled'] = true;
+            }
+        }
+        //$this->SendDebug(__FUNCTION__, $form);
+        return json_encode($form);
+    }
+
+    /**
+     * Overrides the internal IPSModule::ApplyChanges($id) function
+     */
+    public function ApplyChanges()
+    {
+        //Never delete this line!
+        parent::ApplyChanges();
+
+        $name = $this->ReadPropertyString('Name');
+        $coords = json_decode($this->ReadPropertyString('Coords'), true);
+        $this->SendDebug(__FUNCTION__, 'Name: ' . $name . ', Lat:' . $coords['latitude'] . ', Lon:' . $coords['longitude']);
+        if (($name == '') || ($coords['latitude'] == 0) || ($coords['longitude'] == 0)) {
+            $this->SetStatus(201);
+        } else {
+            $this->SetStatus(102);
+        }
+    }
+
+    /**
+     * RequestAction.
+     *
+     *  @param string $ident Ident.
+     *  @param string $value Value.
+     */
+    public function RequestAction($ident, $value)
+    {
+        // Debug output
+        $this->SendDebug(__FUNCTION__, $ident . ' => ' . $value);
+        switch ($ident) {
+            case 'usermap':
+                $this->Usermap($value);
+                break;
+            case 'copy':
+                $this->Copy($value);
+                break;
+            default:
+                // ERROR!!!
+                break;
+        }
+        return true;
+    }
+
+    /**
+     * Reset my registered marker
+     *
+     * @param mixed $id new User/Post ID
+     */
+    public function ResetMyMarker($uid)
+    {
+        $old = $this->ReadAttributeInteger('UserID');
+        if ($uid >= 0) {
+            $this->WriteAttributeInteger('UserID', $uid);
+        }
+        return $old;
+    }
+
+    /**
+     * Hide/unhide form buttons.
+     *
+     */
+    private function ToggleButtons()
+    {
+        $uid = $this->ReadAttributeInteger('UserID');
+        $this->UpdateFormField('btnRegister', 'enabled', ($uid == 0));
+        $this->UpdateFormField('btnUpdate', 'enabled', ($uid != 0));
+        $this->UpdateFormField('btnDelete', 'enabled', ($uid != 0));
+    }
+
+    /**
+     * Register, update or delete user map infos.
+     *
+     * @param bool $value False for transition otherwise true
+     */
+    private function Usermap($value)
+    {
+        $this->SendDebug(__FUNCTION__, $value);
+        // check instance state
+        if ($this->GetStatus() != 102) {
+            $this->SendDebug(__FUNCTION__, 'Status: Instance is not active.');
+            return;
+        }
+        // prepeare header
+        $headers = [
+            'Content-Type: application/json',
+            'Authorization: Basic ' . self::WP_REST_APW,
+        ];
+        // prepeare body
+        $name = $this->ReadPropertyString('Name');
+        $coords = json_decode($this->ReadPropertyString('Coords'), true);
+        $table = json_decode($this->ReadPropertyString('Links'), true);
+        $links = [];
+        foreach ($table as $row) {
+            $links[$row['Type']] = $row['Link'];
+        }
+        $body = [];
+        $body['name'] = $name;
+        $body['coords'] = [$coords['latitude'], $coords['longitude']];
+        if (!empty($links)) {
+            $body['links'] = $links;
+        }
+        $request = json_encode($body);
+        // Action
+        $id = $this->ReadAttributeInteger('UserID');
+        $url = self::WP_REST_URL . '?' . $value . '=' . $id;
+        //$this->SendDebug(__FUNCTION__, 'Request: ' . $url);
+        $response = $this->Request($url, $headers, $request);
+        $text = $this->Translate('Error when calling the function!');
+        if ($response !== false) {
+            $data = json_decode($response, true);
+            if (isset($data['ID'])) {
+                $this->WriteAttributeInteger('UserID', $data['ID']);
+                if (($value == 'delete') || ($data['ID'] != 0)) {
+                    $text = $this->Translate('Function successfully executed!');
+                }
+            }
+        }
+        // Update Buttons
+        $this->ToggleButtons();
+        // Output Message
+        echo $text;
+    }
+
+    /**
+     * Copy the location data from the system in the form.
+     *
+     * @param bool $value No usage
+     */
+    private function Copy($value)
+    {
+        $this->SendDebug(__FUNCTION__, $value);
+        $location = $this->GetLocationData();
+        $this->SendDebug(__FUNCTION__, $location);
+        if (!empty($location)) {
+            $this->UpdateFormField('Coords', 'value', $location);
+        }
+        else {
+            echo $this->Translate('No location data available!');
+        }
+    }
+
+    /**
+     * Returns the users location data stored in symcon.
+     *
+     * @return array location data
+     */
+    private function GetLocationData()
+    {
+        $ids = IPS_GetInstanceListByModuleID(self::LOCATION_CONTROL_GUID);
+        foreach ($ids as $id) {
+            // always the first
+            return IPS_GetProperty($id, 'Location');
+        }
+        return '';
+    }
+
+    /*
+     * Request - Sends the request to the device
+     *
+     * If $request not null, we will send a POST request, else a GET request.
+     * Over the $method parameter can we force a POST or GET request!
+     *
+     * @param string $url Url to call
+     * @param array $header Header information
+     * @param string $request Request data
+     * @param string $mehtod 'GET' od 'POST'
+     * @return mixed response data or false.
+     */
+    private function Request(string $url, array $headers, ?string $request, string $method = 'GET')
+    {
+        //$this->SendDebug(__FUNCTION__, $url, 0);
+        //$this->SendDebug(__FUNCTION__, $headers, 0);
+        // prepeare curl call
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        if ($request != null) {
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $request);
+        } else {
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+        }
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_ENCODING, 'gzip, deflate');
+
+        if (!$response = curl_exec($curl)) {
+            $error = sprintf('Request failed for URL: %s - Error: %s', $url, curl_error($curl));
+            $this->SendDebug(__FUNCTION__, $error, 0);
+        }
+        curl_close($curl);
+        $this->SendDebug(__FUNCTION__, $response, 0);
+        return $response;
+    }
+
+}
